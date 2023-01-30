6352c6ce1856b5843050a84fa7a3d20b1e51d73e
andresguastavino <andresguastavino@gmail.com>
2023-01-30 01:35:41 -0300

    changes to auth form elemnts, styling and design. new logo comp added

--- components/AuthForm/AuthForm.js
+++ components/AuthForm/AuthForm.js
@@ -1,10 +1,11 @@
 import { useRef, useState, useEffect, useContext } from 'react'
 import { FirebaseContext } from '@/contexts/FirebaseContext'
-import { createUserWithEmailAndPassword, signInWithEmailAndPassword, signOut } from 'firebase/auth'
+import { createUserWithEmailAndPassword, signInWithEmailAndPassword } from 'firebase/auth'
 import { useRouter } from 'next/router'
+import Logo from '../Logo/Logo'
 
 export default function AuthForm ({ isRegister, isLogin }) {
-  const [showPassword, setShowPassword] = useState(false)
+  // const [showPassword, setShowPassword] = useState(false)
   const { auth, logged } = useContext(FirebaseContext)
   const router = useRouter()
 
@@ -15,7 +16,7 @@ export default function AuthForm ({ isRegister, isLogin }) {
   const emailInputRef = useRef(null)
   const passwordInputRef = useRef(null)
 
-  const handleSignIn = async (e) => {
+  const handleClick = async (e) => {
     const email = emailInputRef.current.value
     const password = passwordInputRef.current.value
     if (isRegister) {
@@ -41,84 +42,88 @@ export default function AuthForm ({ isRegister, isLogin }) {
     }
   }
 
-  const handleLogOut = async (e) => {
-    await signOut(auth)
-      .then(res => {
-        console.log(res)
-      })
-      .catch(err => {
-        const { code, message } = err
-        console.error(code)
-        console.error(message)
-      })
-  }
-
   return (
-    <section className="container w-screen h-screen flex justify-center items-center">
-      <article className="auth-form w-11/12 md:w-2/5 h-full md:h-3/6 p-4 ">
-        <header className="auth-form-header w-full">
-          <h1>
-            { isRegister && 'Registrate' }
-            { isLogin && 'Logineate' }
-          </h1>
+    <section className="container w-screen h-screen flex justify-center items-center ml-0 mr-0">
+      <article className="brand hidden md:flex md:w-2/4 h-0 md:h-full bg-red-900 p-6 flex-wrap content-base">
+        <Logo classNames={ 'w-2/4 h-2/4 m-0' } />
+          <span className="w-2/4 h-2/4 text-white font-bold flex items-end" style={{ fontSize: '18rem', lineHeight: '16rem' }}>Ch</span>
+          <span className="w-full h-2/4 text-white font-bold" style={{ fontSize: '18rem', lineHeight: '16rem' }}>ipper</span>
+      </article>
+      <article className="auth-form w-11/12 md:w-2/4 h-full md:h-5/6 p-4 flex flex-wrap content-center">
+        <header className="auth-form-header w-full mb-10">
+          <h2 className="text-5xl text-center font-bold py-4 tracking-widest underline">
+            { isRegister ? 'Register' : 'Login' }
+          </h2>
         </header>
-        <main className="auth-form-body flex-row">
-          <div className="auth-form-row">
-            <label
-              className="auth-form-label w-full"
-              htmlFor="email"
-            >
-              Email
-            </label>
-            <input
-              type="email"
-              className="auth-form-input w-full"
-              name="email"
-              id="email"
-              ref={emailInputRef}
-            />
-          </div>
-          <div className="auth-form-row">
-            <label
-              className="auth-form-label w-full"
-              htmlFor="password"
-            >
-              Password
-            </label>
-            <input
-              type="password"
-              className="auth-form-input w-full"
-              name="password"
-              id="password"
-              ref={passwordInputRef}
-            />
-          </div>
+        <main className="auth-form-body flex-row w-full flex flex-wrap justify-center">
+          { isRegister
+            ? <RegisterBody />
+            : <LoginBody emailInputRef={emailInputRef} passwordInputRef={passwordInputRef} />
+          }
         </main>
-        <footer className="auth-form-footer">
-          <button type="button" onClick={handleSignIn}>
-            { isRegister && 'Registrarme' }
-            { isLogin && 'Logearme' }
-          </button>
+        <footer className="auth-form-footer w-full">
+          <div className="auth-form-row w-full flex justify-center items-center">
+            <button
+              className="px-3 py-1 md:px-6 md:py-3 text-white font-bold bg-red-800"
+              type="button"
+              onClick={handleClick}
+            >
+              { isRegister && 'Sign up' }
+              { isLogin && 'Sign in' }
+            </button>
+          </div>
+          <div className="auth-form-row w-full flex justify-start mt-4">
+            <a
+              className="underline text-blue-700"
+              href={ isRegister ? '/auth/login' : '/auth/register' }
+            >
+                { isRegister ? 'Sign in instead' : 'Create an account' }
+            </a>
+          </div>
         </footer>
       </article>
-
-      {/* <h2>{ logged && 'Logeado' }</h2>
-      { logged && <button type="button" onClick={handleLogOut}>X</button>}
-      { isRegister &&
-        <a href='/auth/login'>
-          Mejor me quiero logear porque me acorde que ya tengo cuenta
-        </a>
-      }
-      { isLogin &&
-        <a href='/auth/register'>
-          No tengo cuenta todavia. Me quiero crear una
-        </a>
-      } */}
     </section>
   )
 }
 
-function LoginForm () {
+function LoginBody ({ emailInputRef, passwordInputRef }) {
+  return (
+    <>
+      <div className="auth-form-row my-2 w-full md:w-3/4 flex flex-wrap flex-col mx-auto content-center">
+        <label
+          className="auth-form-label w-full md:w-2/4"
+          htmlFor="email"
+        >
+          Email
+        </label>
+        <input
+          type="email"
+          className="auth-form-input w-full md:w-2/4 mt-1"
+          name="email"
+          id="email"
+          ref={emailInputRef}
+        />
+      </div>
+      <div className="auth-form-row my-2 w-full md:w-3/4 flex flex-wrap flex-col mx-auto content-center">
+        <label
+          className="auth-form-label w-full md:w-2/4"
+          htmlFor="password"
+        >
+          Password
+        </label>
+        <input
+          type="password"
+          className="auth-form-input w-full md:w-2/4 mt-1"
+          name="password"
+          id="password"
+          ref={passwordInputRef}
+        />
+      </div>
+    </>
+  )
+}
+
+function RegisterBody () {
   return (
     <>
     </>
